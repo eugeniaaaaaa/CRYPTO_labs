c99d5129541c89fd3ac05a4a6f61f724d4d9b597
eugeniaaaaaa <gurzievgenia@gmail.com>
2022-01-09 19:45:02 +0200

    Task1 done, start writing task2

---
+++ Lab1/src/main/java/crypto/lab1/Task2.java
@@ -0,0 +1,15 @@
+package crypto.lab1;
+
+public class Task2 {
+    // According to the Concise Oxford Dictionary
+    private static final char[] symbolFrequenciesAscending = " EARIOTNSLCUDPMHGBFYWKVXZJQ".toLowerCase().toCharArray();
+
+    public static void main(String[] args) {
+        final String text = "Now the first one who will post the link to this document to our chat will receive +1 score. Simple.";
+
+    }
+
+    private static byte[] encode() {
+        return null;
+    }
+}
