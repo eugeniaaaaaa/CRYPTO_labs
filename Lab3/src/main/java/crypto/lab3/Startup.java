50b4bfc1abc025191ac15348afd8e57b68535386
eugeniaaaaaa <gurzievgenia@gmail.com>
2022-01-11 00:34:35 +0200

    Lab 3 task 2 possible solution

--- Lab3/src/main/java/crypto/lab3/Startup.java
+++ Lab3/src/main/java/crypto/lab3/Startup.java
@@ -1,13 +1,17 @@
 package crypto.lab3;
 
 import crypto.lab3.random.LcgBreaker;
+import crypto.lab3.random.MtBreaker;
 import crypto.lab3.random.RandomBreaker;
 import crypto.lab3.remote.GameMode;
 
+import java.util.Random;
+
 public class Startup {
     public static void main(String[] args) {
-        RemoteService service = new RemoteService("http://95.217.177.249/casino", 5431, GameMode.LCG);
-        RandomBreaker breaker = new LcgBreaker(service);
+        RemoteService service = new RemoteService("http://95.217.177.249/casino", 5434, GameMode.LCG);
+//        RandomBreaker breaker = new LcgBreaker(service);
+        RandomBreaker breaker = new MtBreaker(service);
         breaker.becomeRich();
     }
 }
--- Lab3/src/main/java/crypto/lab3/random/MtBreaker.java
+++ Lab3/src/main/java/crypto/lab3/random/MtBreaker.java
@@ -1,6 +1,11 @@
 package crypto.lab3.random;
 
 import crypto.lab3.RemoteService;
+import crypto.lab3.utils.Mt19937Random;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class MtBreaker extends AbstractBreaker {
     public MtBreaker(RemoteService remoteService) {
@@ -9,6 +14,22 @@ public class MtBreaker extends AbstractBreaker {
 
     @Override
     public void becomeRich() {
+        final long start = System.currentTimeMillis() / 1000;
+        final long realNumber = getRemoteService().play(1, 1L).getRealNumber();
+        final long end = System.currentTimeMillis() / 1000;
 
+        final int marginError = 10;
+        Mt19937Random mtOnServer = LongStream.range(start - marginError, end + marginError)
+                .mapToInt(i -> (int) i)
+                .mapToObj(Mt19937Random::new)
+                .filter(mt -> realNumber == mt.next())
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+
+        long money;
+        do {
+            long next = mtOnServer.next();
+            money = getRemoteService().play(100, next).getAccount().getMoney();
+        } while (money < 1_000_000);
     }
 }
