72956dd8746a1d7793782511261988885960c676
eugeniaaaaaa <gurzievgenia@gmail.com>
2022-01-11 02:35:56 +0200

    Move password files, PasswordGeneratorStartup

---
+++ Lab4/src/main/java/crypto/lab4/PasswordGeneratorStartup.java
@@ -0,0 +1,31 @@
+package crypto.lab4;
+
+import com.opencsv.CSVWriter;
+import lombok.SneakyThrows;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.function.BiConsumer;
+
+public class PasswordGeneratorStartup {
+    public static void main(String[] args) {
+        PasswordGenerator generator = new PasswordGenerator();
+        writeGeneratedPasswords("data/md5.csv", generator, (password, writer) ->
+                writer.writeNext(new String[] {DigestUtils.md5Hex(password).toLowerCase()}));
+        writeGeneratedPasswords("data/sha1-with-salt.csv", generator, (password, writer) -> {
+            String salt = generator.randomPassword();
+            writer.writeNext(new String[] {DigestUtils.sha1Hex(password + salt).toLowerCase(Locale.ROOT), salt});
+        });
+    }
+
+    @SneakyThrows
+    private static void writeGeneratedPasswords(String fileName, PasswordGenerator generator, BiConsumer<String, CSVWriter> entryWriter) {
+        try (CSVWriter writer = new CSVWriter(Files.newBufferedWriter(Paths.get(fileName)))) {
+            for (int i = 0; i < 100_000; i++) {
+                entryWriter.accept(generator.generatePassword(), writer);
+            }
+        }
+    }
+}
