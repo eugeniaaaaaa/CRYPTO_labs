80e92efca4478ed3af870ffb8a45bdbb61760943
eugeniaaaaaa <gurzievgenia@gmail.com>
2022-01-11 20:28:02 +0200

    Write common passwords to database

--- Labs567/src/main/java/crypto/labs567/Startup.java
+++ Labs567/src/main/java/crypto/labs567/Startup.java
@@ -1,9 +1,12 @@
 package crypto.labs567;
 
+import crypto.labs567.properties.CryptoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CryptoProperties.class)
 public class Startup {
     public static void main(String[] args) {
         SpringApplication.run(Startup.class);
---
+++ Labs567/src/main/java/crypto/labs567/properties/CryptoProperties.java
@@ -0,0 +1,12 @@
+package crypto.labs567.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "crypto")
+@Getter
+@Setter
+public class CryptoProperties {
+    private String commonPasswordsFileName;
+}
